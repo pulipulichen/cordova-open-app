d61b1daf5f2ee276d1d9f56b91d16e3ba15300cc
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-11-04 20:39:24 +0800

    西元2024年11月04日 (週一) 20時39分24秒 CST

--- www/script.js
+++ www/script.js
@@ -482,7 +482,23 @@ function ready() {
           action: "com.google.zxing.client.android.SCAN",
           category: "android.intent.category.DEFAULT",
       };
-      openWebIntent(config)
+      // openWebIntent(config)
+
+      window.plugins.webintent.startActivity(config,
+        callbackExitApp,
+        (e) => {
+          cordova.plugins.backgroundMode.moveToForeground();
+          alert('Failed:' + JSON.stringify(e, null, 2));
+
+          // navigator.app.exitApp();
+          config = {
+            action: window.plugins.webintent.ACTION_VIEW,
+            url: `https://play.google.com/store/apps/details?id=com.google.zxing.client.android`
+          };
+
+          openWebIntent(config)
+        }
+    );
   
     //     var sApp = startApp.set({
     //         "package": "com.google.zxing.client.android.SCAN",
